64e4e858156aa269121671bef935d11d10bc2fd3
SethSR <srenshaw1@gmail.com>
2015-05-21 11:33:23 -0500

    initial commit

---
+++ src/OptionalType.hx
@@ -0,0 +1,20 @@
+enum OptType {
+	Stereo; //               extends OptType { override val toString = "Stereo" }
+	Liftwire; //             extends OptType { override val toString = "Liftwire" }
+	AntiTheftCodeLock; //    extends OptType { override val toString = "Anti-Theft Code Lock" }
+	Spotlights; //           extends OptType { override val toString = "Spotlights" }
+	Nightlights; //          extends OptType { override val toString = "Nightlights" }
+	StorageModule; //        extends OptType { override val toString = "Storage Module" }
+	Micromanipulators; //    extends OptType { override val toString = "Micromanipulators" }
+	SlickSpray; //           extends OptType { override val toString = "Slick-Spray" }
+	BoggSpray; //            extends OptType { override val toString = "Bogg-Spray" }
+	DamageControlPackage; // extends OptType { override val toString = "Damage Control Package" }
+	QuickChangeMount; //     extends OptType { override val toString = "Quick Change Mount" }
+	SilentRunning; //        extends OptType { override val toString = "Silent Running" }
+	Parachute; //            extends OptType { override val toString = "Parachute" }
+	ReEntryPackage; //       extends OptType { override val toString = "Re-Entry Package" }
+	EjectionSeat; //         extends OptType { override val toString = "Ejection Seat" }
+	EscapePod; //            extends OptType { override val toString = "Escape Pod" }
+	ManeuverPod; //          extends OptType { override val toString = "Maneuver Pod" }
+	VehiclePod; //           extends OptType { override val toString = "Vehicle Pod" }
+}
