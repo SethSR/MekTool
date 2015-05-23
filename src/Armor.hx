bc4df635161ef61b736834cfc3fa5c878ad1c24e
Seth Renshaw <srenshaw1@gmail.com>
2015-05-22 19:31:09 -0500

    Changed ArmorClass to simple enum

--- src/Armor.hx
+++ src/Armor.hx
@@ -1,13 +1,9 @@
-import com.mindrocks.functional.Functional;
-
-using MekParser;
-
 enum ArmorClass {
-	Ablative(cost_mod: Float, dmg_coeff: Int);
-	Standard(cost_mod: Float, dmg_coeff: Int);
-	Alpha   (cost_mod: Float, dmg_coeff: Int);
-	Beta    (cost_mod: Float, dmg_coeff: Int);
-	Gamma   (cost_mod: Float, dmg_coeff: Int);
+	Ablative;
+	Standard;
+	Alpha;
+	Beta;
+	Gamma;
 }
 
 enum ArmorType {
@@ -16,39 +12,57 @@ enum ArmorType {
 }
 
 class Armor {
-	var armor_type: ArmorType;
-	var size_class: SizeClass;
-	var armor_class: ArmorClass;
-	var ram_value_1: Int = 0;
-	var ram_value_2: Int = 0;
+	var armorType: ArmorType;
+	var sizeClass: SizeClass;
+	var armorClass: ArmorClass;
+	var ramValue1: Int = 0;
+	var ramValue2: Int = 0;
+	var cost: Float;
+	var stoppingPower: Float;
+	var damageCoefficient: Float;
 	public function new(at: ArmorType, sc: SizeClass, ac: ArmorClass, r1: Int = 0, r2: Int = 0) {
-		armor_type = at;
-		size_class = sc;
-		armor_class = ac;
-		ram_value_1 = r1;
-		ram_value_2 = r2;
+		armorType = at;
+		sizeClass = sc;
+		armorClass = ac;
+		ramValue1 = r1;
+		ramValue2 = r2;
+
+		var armor_cost_mod = switch (ac) {
+			case Ablative: 0.50;
+			case Standard: 1.00;
+			case Alpha   : 1.25;
+			case Beta    : 1.50;
+			case Gamma   : 2.00;
+		}
+
+		cost = (1 + sizeClass) * armor_cost_mod * switch (r2) {
+			case 5: 1.5;
+			case 4: 1.8;
+			case 3: 2.2;
+			case 2: 2.5;
+			case _: 1.0;
+		}
+
+		stoppingPower = (1 + sizeClass) * switch (r2) {
+			case 5: 1.00;
+			case 4: 0.80;
+			case 3: 0.75;
+			case 2: 0.67;
+			case _: 1.00;
+		}
+
+		damageCoefficient = switch (ac) {
+			case Ablative: 0;
+			case Standard: 1;
+			case Alpha   : 2;
+			case Beta    : 4;
+			case Gamma   : 8;
+		}
 	}
 
-  var cost = (1 + size_class.value) * armor_class.cost_mod * switch (ram_value_2) {
-    case 5: 1.5;
-    case 4: 1.8;
-    case 3: 2.2;
-    case 2: 2.5;
-    case _: 1.0;
-  }
-  var stopping_power = (1 + size_class.value) * switch (ram_value_2) {
-    case 5: 1.00;
-    case 4: 0.80;
-    case 3: 0.75;
-    case 2: 0.67;
-    case _: 1.00;
-  }
+  // var toString =
+  //   details('$sizeClass $armorClass $armorType' +
+  //   (if (armor_type == RAMArmor) ' $ramValue1/$ramValue2' else ''), 4);
 
-  var damage_coefficient = armor_class.dmg_coeff;
-
-  var toString =
-    details('$size_class $armor_class $armor_type' +
-    (if (armor_type == RAMArmor) ' $ram_value_1/$ram_value_2' else ''), 4);
-
-  function details(s: String, n: Int = 0) return '${comment_offset-n}s = (cost : %6.2f | SP    : %6.2f | DC    : %3d)'.format(s,cost,stopping_power,damage_coefficient);
+  // function details(s: String, n: Int = 0) return '%-${39-n}s = (cost : %6.2f | SP    : %6.2f | DC    : %3d)'.format(s,cost,stoppingPower,damageCoefficient);
 }
