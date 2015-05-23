f2a73e5033a86d990db56c1015ccb837bf1fc8ec
Seth Renshaw <srenshaw1@gmail.com>
2015-05-22 19:33:47 -0500

    changed SizeClass to an abstract enum

--- src/SizeClass.hx
+++ src/SizeClass.hx
@@ -1,13 +1,24 @@
-enum SizeClass {
-	Superlight;
-	Lightweight;
-	Striker;
-	MediumStriker;
-	HeavyStriker;
-	Mediumweight;
-	LightHeavy;
-	MediumHeavy;
-	ArmoredHeavy;
-	SuperHeavy;
-	MegaHeavy;
+@:enum
+abstract SizeClass(Int) from Int to Int {
+	var Superlight    =  0;
+	var Lightweight   =  1;
+	var Striker       =  2;
+	var MediumStriker =  3;
+	var HeavyStriker  =  4;
+	var Mediumweight  =  5;
+	var LightHeavy    =  6;
+	var MediumHeavy   =  7;
+	var ArmoredHeavy  =  8;
+	var SuperHeavy    =  9;
+	var MegaHeavy     = 10;
+
+	@:op(A + B)
+	public function add(i: Int) {
+		return this + i;
+	}
+
+	@:op(A + B)
+	static public function add2(i: Int, s: SizeClass) {
+		return s.add(i);
+	}
 }
