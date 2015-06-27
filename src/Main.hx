f79cb504f08bcbfd86e789a3f33e929799e2b0af
Seth Renshaw <srenshaw1@gmail.com>
2015-06-27 16:22:09 -0500

    Output parse result to a file

--- src/Main.hx
+++ src/Main.hx
@@ -2,9 +2,10 @@ import sys.io.File;
 
 class Main {
     static public function main() {
+    	trace ('');
     	// var test = new haxe.unit.TestRunner();
     	// test.add(new MekParser.MekTest());
     	// test.run();
-    	MekParser.test([for (str in Sys.args()) File.getContent(str)].join('\n'));
+    	File.saveContent('output.mtz', MekParser.test([for (str in Sys.args()) File.getContent(str)].join('\n')));
     }
 } //Main
